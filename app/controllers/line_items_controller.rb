513594512835e5a32909e8c170a87b037edfe094
rocknrollMarc <marc.bluemner@gmail.com>
2013-12-20 14:55:27 +0100

    ajax

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -67,12 +67,10 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
-    @line_item = LineItem.find(params[:id])
     @line_item.destroy
-    
     respond_to do |format|
-      format.html { redirect_to(@line_item.cart, :notice => 'Item has been removed from your cart.') }
-      format.xml{ head  :ok }
+      format.html { redirect_to line_items_url }
+      format.json { head :no_content }
     end
   end
 
