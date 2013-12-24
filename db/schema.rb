4af896de2bacf30f4adb0410e2dbc541d3613c6a
rocknrollMarc <marc.bluemner@gmail.com>
2013-12-24 13:28:17 +0100

    login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131223151404) do
+ActiveRecord::Schema.define(version: 20131224121853) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -48,6 +48,13 @@ ActiveRecord::Schema.define(version: 20131223151404) do
     t.string   "image_url"
     t.string   "string"
     t.decimal  "price",       precision: 8, scale: 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
